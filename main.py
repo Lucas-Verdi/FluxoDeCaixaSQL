4f5cffd5a06c649a4ca2729a5fbf884c6eff60bc
Lucas <lucas.a.verdi@gmail.com>
2023-07-26 11:26:09 -0300

    Update

--- main.py
+++ main.py
@@ -27,6 +27,12 @@ sobrasbb = []
 
 sobrasgt = []
 
+arquivocontas = None
+datascontas = []
+valorescontas = []
+valorescontasf = []
+contasfinal = []
+
 
 def create_server_connection(host_name, user_name, user_password):
     connection = None
@@ -71,6 +77,12 @@ def ler2():
     labelbt2 = Label(janela, text="{} CARREGADO".format(arquivocbb), font="Arial 7")
     labelbt2.grid(column=0, row=4)
 
+def ler3():
+    global arquivocontas
+    arquivocontas = filedialog.askopenfilename()
+    labelbt3 = Label(janela, text="{} CARREGADO".format(arquivocontas), font="Arial 7")
+    labelbt3.grid(column=0, row=6)
+
 def truncate():
     connection = create_server_connection("localhost", "root", "wolf")
     usardb = "USE fluxodecaixa"
@@ -79,12 +91,14 @@ def truncate():
     truncate3 = "TRUNCATE TABLE resultsbb"
     truncate4 = "TRUNCATE TABLE distinctbb"
     truncate5 = "TRUNCATE TABLE final"
+    truncate6 = "TRUNCATE TABLE contas"
     execute_query(connection, usardb)
     execute_query(connection, truncate)
     execute_query(connection, truncate2)
     execute_query(connection, truncate3)
     execute_query(connection, truncate4)
     execute_query(connection, truncate5)
+    execute_query(connection, truncate6)
 
 def start():
     a = Th(1)
@@ -112,11 +126,17 @@ class Th(Thread):
         pastadetrabalhocbb = xlwings.Book(arquivocbb)
         planilhacbb = pastadetrabalhocbb.sheets['Planilha1']
 
+        pastadetrabalhocontas = xlwings.Book(arquivocontas)
+        planilhacontas = pastadetrabalhocontas.sheets[0]
+
         last_row = planilha.range('B1').end('down').row
         last_row2 = planilha.range('A1').end('down').row
 
         last_row3 = planilhacbb.range('A1').end('down').row
         last_row4 = planilhacbb.range('E1').end('down').row
+
+        last_row5 = planilhacontas.range('A7').end('down').row
+        last_row6 = planilhacontas.range('F7').end('down').row
 
         for a in range(1, last_row3 + 1):
             valtemp = planilhacbb.range('A{}'.format(a)).value
@@ -138,9 +158,21 @@ class Th(Thread):
             date = datetime.strptime(datasgetnet[c], '%d/%m/%Y').date()
             datastr.append(date)
 
+        for cdt in range(7, last_row5 + 1):
+            valtemp = planilhacontas.range('A{}'.format(cdt)).value
+            datascontas.append(valtemp)
+
+        for cv in range(7, last_row6 + 1):
+            valtemp = planilhacontas.range('F{}'.format(cv)).value
+            valorescontas.append(valtemp)
+
+        for cvf in range(0, len(valorescontas)):
+            valtemp = float(valorescontas[cvf])
+            valorescontasf.append(valtemp)
+
 
         connection = create_server_connection("localhost", "root", "wolf")
-        print(datastr)
+
 
         for j in range(0, len(valoresgetnet)):
             usardb = "USE fluxodecaixa"
@@ -151,6 +183,12 @@ class Th(Thread):
         for y in range(0, len(valorescbb)):
             usardb = "USE fluxodecaixa"
             inserir = "INSERT INTO bbrasil (dataatual, valor) VALUES ('{}', '{}')".format(datascbb[y], valorescbb[y])
+            execute_query(connection, usardb)
+            execute_query(connection, inserir)
+
+        for ct in range(0, len(valorescontasf)):
+            usardb = "USE fluxodecaixa"
+            inserir = "INSERT INTO contas (data, valor) VALUES ('{}', '{}')".format(datascontas[ct], valorescontasf[ct])
             execute_query(connection, usardb)
             execute_query(connection, inserir)
 
@@ -191,13 +229,22 @@ class Th(Thread):
         cursor3.execute("SELECT dataatual, valor FROM getnet WHERE dataatual NOT IN (select dataatual FROM distinctbb);")
         sobrasgt = cursor3.fetchall()
 
+        cursor4 = conn.cursor()
+        cursor4.execute('SELECT DISTINCT data, sum(valor) OVER (PARTITION BY data ORDER BY data) AS soma_acumulada FROM contas ORDER BY data;')
+        contasfinal = cursor4.fetchall()
+
         last_row = sheet.range('A1').end('down').row
         sheet.range("A{}".format(last_row + 1)).value = sobrasbb
         last_row2 = sheet.range('A1').end('down').row
         sheet.range("A{}".format(last_row2 + 1)).value = sobrasbb = sobrasgt
 
+        sheet.range('C1').value = contasfinal
+
         pastadetrabalhogetnet.close()
         pastadetrabalhocbb.close()
+        pastadetrabalhocontas.close()
+        print(valorescontasf)
+
 
 
 
@@ -220,12 +267,16 @@ botao2 = Button(janela, text="COBRANCABB", font="Arial 10")
 botao2.grid(column=0, row=2, padx=10, pady=10)
 botao2.bind("<Button>", lambda e: ler2())
 
+botao4 = Button(janela, text="CONTAS A PAGAR", font="Arial 10")
+botao4.grid(column=0, row=5, padx=10, pady=10)
+botao4.bind("<Button>", lambda e: ler3())
+
 botao3 = Button(janela, text="GERAR CONTROLE", font="Arial 10")
-botao3.grid(column=0, row=5, padx=10, pady=10)
+botao3.grid(column=0, row=7, padx=10, pady=10)
 botao3.bind("<Button>", lambda e: start())
 
 truncatebtn = Button(janela, text="TRUNCATE", font="Arial 10")
-truncatebtn.grid(column=0, row=6, padx=10, pady=10)
+truncatebtn.grid(column=0, row=8, padx=10, pady=10)
 truncatebtn.bind("<Button>", lambda e: truncate())
 
 janela.mainloop()
